0d5ec03cdd1e350dfb4a03a744ca0459a969469d
Yu-Keu <penerimaanpyit@gmail.com>
2023-05-09 10:29:44 +0700

    Restructure home element

--- src/home.js
+++ src/home.js
@@ -1,10 +1,19 @@
 export default function home () {
   const container = document.getElementById('content')
-  const header = document.createElement('h1')
-  const desc = document.createElement('h2')
 
+  const header = document.createElement('h1')
   header.textContent = 'Savoria'
+
+  const description = document.createElement('div')
+
+  const tagline = ['Where flavor meets fun!', 'Our restaurant is all about great food, good vibes, and unforgettable memories']
+  tagline.forEach((text) => {
+    const paragraph = document.createElement('h2')
+    paragraph.textContent = text
+    description.append(paragraph)
+  })
+
   header.classList.add('head')
-  desc.textContent = 'Where flavor meets fun! Welcome to Savoria - our restaurant is all about great food, good vibes, and unforgettable memories. Come join the feast!'
-  container.append(header, desc)
+
+  container.append(header, description)
 }
